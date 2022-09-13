1da41131cc59409d2c5afeae75d817855c07a46b
samin <saminbinkarim@gmail.com>
2022-09-13 16:13:40 +0600

    Car image change

--- bitmapToText/imgToLogicMap.py
+++ bitmapToText/imgToLogicMap.py
@@ -1,26 +1,28 @@
 import numpy as np
 from matplotlib.image import imread
 
-
-
-colorToSymbolDict = {"#ffffff" : "*",
+colorToSymbol = {"#ffffff": "*",
                      "#ff0000": "p",
                      "#000000": " "}
 
-def colorToSymbolMap(colorString ,colorToSymbolDict = colorToSymbolDict):
+
+def colorToSymbolMap(colorString, colorToSymbolDict=colorToSymbol):
     if colorString in colorToSymbolDict.keys():
         return colorToSymbolDict[colorString]
     else:
         print("invalid color entry, spawning empty space")
         return " "
 
+
 def clamp(x):
     x = int(x * 255)
     return max(0, min(x, 255))
 
+
 def rgbToSymbol(r, g, b):
     colorHexString = "#{0:02x}{1:02x}{2:02x}".format(clamp(r), clamp(g), clamp(b))
     return colorToSymbolMap(colorHexString)
+
 
 def main():
     img = imread("img.png")
@@ -28,10 +30,11 @@ def main():
     for line in img:
         myLine = ""
         for pixel in line:
-            r,g,b = pixel[0], pixel[1], pixel[2]
-            myLine += rgbToSymbol(r,g,b)
+            r, g, b = pixel[0], pixel[1], pixel[2]
+            myLine += rgbToSymbol(r, g, b)
         symbolMap = symbolMap + myLine + "\n"
     print(symbolMap)
+
 
 if __name__ == "__main__":
     main()
--- dmlab2d/lib/game_scripts/levels/traffic_norms/simulation.lua
+++ dmlab2d/lib/game_scripts/levels/traffic_norms/simulation.lua
@@ -24,23 +24,6 @@ local Simulation = class.Class()
 function Simulation.defaultSettings()
   return {
       mapName = 'default',
-      -- Probability apple.possible becomes apple.
-      initialAppleProbability = 0.75,
-      -- Displays a color sprite associated with the probability of respawning.
-      -- Green is highest probability red it zero probability.
-      showRespawnProbability = false,
-      -- Base probability of an apple respawning each step.
-      appleRespawnProbabilities = setmetatable({0, 0.001, 0.005, 0.025}, {
-        __index = function(table, key)
-          local key = tonumber(key)
-          if key == nil then
-            return nil
-          end
-          table[key] = 0
-          return 0
-        end}),
-      -- Seeding radius.
-      seedRadius = 3
   }
 end
 
@@ -48,38 +31,10 @@ function Simulation:__init__(kwargs)
   self._settings = kwargs.settings
 end
 
-local function interpColor(colors, fraction)
-  local place = fraction * (#colors - 1) + 1
-  local from = math.floor(place)
-  local frac = place - from
-  local to = math.min(#colors, from + 1)
-  local result = {}
-  for i = 1, # colors do
-    result[i] = math.floor(colors[from][i] * (1 - frac) + colors[to][i] * frac)
-  end
-  return result
-end
-
 function Simulation:addSprites(tileSet)
   tileSet:addColor('OutOfBounds', {0, 0, 0})
   tileSet:addColor('OutOfView', {80, 80, 80})
   tileSet:addShape('Wall', images.wall())
-  tileSet:addShape('Apple', images.apple())
-  if self._settings.showRespawnProbability then
-    -- Use probability to color respawn tile.
-    local colors = {{255, 0, 0}, {255, 255, 0}, {0, 255, 0}}
-    local count = #self._settings.appleRespawnProbabilities
-    local maxP = 1e-3
-    for i, p in ipairs(self._settings.appleRespawnProbabilities) do
-      if p > maxP then
-        maxP = p
-      end
-    end
-    for i, p in ipairs(self._settings.appleRespawnProbabilities) do
-      local color = interpColor(colors, math.sqrt(p) / math.sqrt(maxP))
-      tileSet:addColor('apple.wait.' .. i, color)
-    end
-  end
 end
 
 function Simulation:worldConfig()
@@ -106,18 +61,6 @@ function Simulation:worldConfig()
       }
   }
   local waitNames = {}
-  for i, prob in ipairs(settings.appleRespawnProbabilities) do
-    local name = 'apple.wait.' .. i
-    local spriteName = settings.showRespawnProbability and name or nil
-    waitNames[i] = name
-    table.insert(config.updateOrder, {name = name, func = 'respawn'})
-    local waiters = prob > 0 and 'waiter'
-    config.states['apple.wait.' .. i] = {
-        layer = 'wait',
-        groups = {name},
-        sprite = spriteName,
-    }
-  end
   self._waitNames = waitNames
 
   if settings.showRespawnProbability then
@@ -187,47 +130,11 @@ function Simulation:stateCallbacks(avatars)
     end
   end
 
-  local appleW = {}
-  function appleW.onAdd(grid, appleWait)
-      local pos = grid:position(appleWait)
-      local waitNames = self._waitNames
-      local count = 1
-      for piece in pairs(grid:queryDiamond('logic', pos, radius)) do
-        count = count + 1
-        if count == #waitNames then
-          break
-        end
-      end
-      grid:setState(appleWait, waitNames[count])
-  end
-
-  local appleWaitCallback = {onUpdate = {}}
-  appleWaitCallback.onUpdate.respawn = function(grid, piece)
-    grid:setState(piece, 'apple')
-  end
-
-  local applePossible = {}
-  local initialAppleProbability = settings.initialAppleProbability
-  function applePossible.onAdd(grid, piece)
-    if random:uniformReal(0, 1) <= initialAppleProbability then
-      grid:setState(piece, 'apple')
-    end
-  end
-
-  stateCallbacks.apple = apple
-  stateCallbacks['apple.wait'] = appleW
-  for i = 1, #self._settings.appleRespawnProbabilities do
-    stateCallbacks['apple.wait.' .. i] = appleWaitCallback
-  end
-  stateCallbacks['apple.possible'] = applePossible
   return stateCallbacks
 end
 
 function Simulation:start(grid)
-  for i, prob in ipairs(self._settings.appleRespawnProbabilities) do
-    local name = 'apple.wait.' .. i
-    grid:setUpdater{update = name, group = name, probability = prob}
-  end
+
 end
 
 function Simulation:update(grid) end
