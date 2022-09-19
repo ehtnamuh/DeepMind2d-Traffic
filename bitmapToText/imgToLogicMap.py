d8ba6f1d1f07bf50a9d6222864de2da804118c3f
samin <saminbinkarim@gmail.com>
2022-09-19 15:18:36 +0600

    saving logic maps to text files

--- bitmapToText/imgToLogicMap.py
+++ bitmapToText/imgToLogicMap.py
@@ -1,16 +1,18 @@
 from matplotlib.image import imread
 
-colorToSymbol = {"#ffffff": "*",
-                    "#ff0000": "b",
-                    "#00ff00": "e",
-                    "#0000ff": "n",
-                    "#ffff00": "p",
-                    "#000000": " "}
+color_to_symbol = {"#ffffff": "*",
+                   "#ff0000": "b",
+                   "#00ff00": "e",
+                   "#0000ff": "n",
+                   "#ffff00": "p",
+                   "#000000": " "}
 
 
-def colorToSymbolMap(colorString, colorToSymbolDict=colorToSymbol):
-    if colorString in colorToSymbolDict.keys():
-        return colorToSymbolDict[colorString]
+def color_to_symbol_map(color_string, color_to_symbol_dict=None):
+    if color_to_symbol_dict is None:
+        color_to_symbol_dict = color_to_symbol
+    if color_string in color_to_symbol_dict.keys():
+        return color_to_symbol_dict[color_string]
     else:
         print("invalid color entry, spawning empty space")
         return " "
@@ -21,22 +23,35 @@ def clamp(x):
     return max(0, min(x, 255))
 
 
-def rgbToSymbol(r, g, b):
-    colorHexString = "#{0:02x}{1:02x}{2:02x}".format(clamp(r), clamp(g), clamp(b))
-    return colorToSymbolMap(colorHexString)
+def rgb_to_symbol(r, g, b):
+    color_hex_string = "#{0:02x}{1:02x}{2:02x}".format(clamp(r), clamp(g), clamp(b))
+    return color_to_symbol_map(color_hex_string)
 
 
-def main():
-    # img = imread("roadLogic.png")
-    img = imread("playerSpawns.png")
-    symbolMap = ""
+def img_to_logic_map(img):
+    symbol_map = ""
     for line in img:
         myLine = ""
         for pixel in line:
             r, g, b = pixel[0], pixel[1], pixel[2]
-            myLine += rgbToSymbol(r, g, b)
-        symbolMap = symbolMap + myLine + "\n"
-    print(symbolMap)
+            myLine += rgb_to_symbol(r, g, b)
+        symbol_map = symbol_map + myLine + "\n"
+    return symbol_map
+
+
+def write_map_to_file(logic_map, map_name):
+    f = open(map_name, "w")
+    f.write(logic_map)
+    f.close()
+
+
+def main():
+    img = imread("playerSpawns.png")
+    logic_map = img_to_logic_map(img)
+    write_map_to_file(logic_map, "playerSpawns.txt")
+    img = imread("roadLogic.png")
+    logic_map = img_to_logic_map(img)
+    write_map_to_file(logic_map, "roadLogic.txt")
 
 
 if __name__ == "__main__":
